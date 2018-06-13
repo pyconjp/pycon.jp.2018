249f550b13ee6f2219d38aa5c0389d8129d60cbd
Yasushi HASHIMOTO <yhashimoto@hashimoto-no-iMac.local>
2018-06-13 14:37:47 +0900

    Vueに対応させる

--- components/TopSponsor/index.vue
+++ components/TopSponsor/index.vue
@@ -2,6 +2,24 @@
 
 <script>
   export default {
-    name: 'top-sponsor'
+    name: 'top-sponsor',
+    data(){
+      return {
+        // Todo: 本番ではStoreからAPIのデータを取得
+        sponsor_array : [
+          {
+            link: '#',
+            image: 'https://placehold.jp/190x100.png',
+            alt: 'スポンサー1',
+
+          },
+          {
+            link: '#',
+            image: 'https://placehold.jp/190x100.png',
+            alt: 'スポンサー2',
+          },
+        ]
+      }
+    }
   }
 </script>
