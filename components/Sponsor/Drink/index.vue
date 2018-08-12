4458771708b1d8762d8d20814af0fae2efcf36d3
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2018-08-12 12:53:37 +0900

    Add DrinkSponsor

--- components/Sponsor/Drink/index.vue
+++ components/Sponsor/Drink/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  section#LunchSponsor.sponsor-section
-      h2 Lunch Sponsor
+  section#DrinkSponsor.sponsor-section
+      h2 Drink Sponsor
       .card-block.uk-flex.uk-flex-wrap
         small-card(v-for="(sponsor, index) in sponsors" :index="index" :key="sponsor.id" :sponsor="sponsor")
 </template>
@@ -8,7 +8,7 @@
 <script>
   import SmallCard from '~/components/Sponsor/Card/SmallCard';
   export default {
-    name: 'lunch-sponsor',
+    name: 'drink-sponsor',
     props: ["sponsors"],
     components: {
       SmallCard,
