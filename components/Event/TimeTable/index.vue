5ed4cbc3ff9a419249dc46df90f32675942fe55b
matsuko-c <s_matsuda@c-cube-design.jp>
2018-08-19 16:13:02 +0900

    event/time-scheduleのテーブルデザイン（ほぼPC向け）を追加

--- components/Event/TimeTable/index.vue
+++ components/Event/TimeTable/index.vue
@@ -1,16 +1,37 @@
 <template src="./table.pug" lang="pug"/>
+<style src="./table.sass" lang="sass" />
 
 <script>
-  export default {
-    name: 'time-table',
-    methods:{
-      parallelScroll: function (evt, el) {
-           const tableHeader = document.getElementById('test')
-           const sLeft = el.scrollLeft
-           tableHeader.scrollLeft = sLeft
+export default {
+  name: 'time-table',
+  data: () => {
+    return{
+      isFixed: false,
+    }
+  },
+  methods:{
+    parallelScroll: function (evt, el) {
+      let tableHeader = document.getElementById('test')
+      if(window.scrollY > (112+90)){
+        const sLeft = el.scrollLeft
+        tableHeader.scrollLeft = sLeft
+      }else{
+        //const tableHeader = document.getElementById('test')
+        tableHeader.scrollLeft = 0
+      }
+    },
+    fixedHead: function(evt, el) {
+      let tableHeader = document.getElementById('test')
+      if(window.scrollY > (112+89)){
+        this.$data.isFixed = true
+        const sLeft = el.scrollLeft
+        tableHeader.scrollLeft = sLeft
+      }else{
+        this.$data.isFixed = false
+        //const tableHeader = document.getElementById('test')
+        tableHeader.scrollLeft = 0
       }
     }
   }
+}
 </script>
-
-<style src="./table.sass" lang="sass" />
