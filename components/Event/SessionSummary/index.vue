b08da4f8dcb5635aac3b85adc6af9888566c5740
matsuko-c <s_matsuda@c-cube-design.jp>
2018-08-23 22:56:16 +0900

    タイムテーブルのトークデータAPI接続

---
+++ components/Event/SessionSummary/index.vue
@@ -0,0 +1,35 @@
+<template src="./summary.pug" lang="pug"/>
+<style src="./summary.sass" lang="sass" />
+<script>
+  export default {
+    name: 'session-summary',
+    props: ['talk'],
+    computed: {
+      langage() {
+        return (this.talk && this.talk.hasOwnProperty('lang')) ?  this.talk.lang.toUpperCase() : ""
+      },
+      room() {
+        return (this.talk && this.talk.hasOwnProperty('room')) ?  this.talk.room : ""
+      },
+      talk_format() {
+        return (this.talk && this.talk.hasOwnProperty('talk_format')) ?  this.talk.talk_format + "min" : ""
+      },
+      title() {
+        return (this.talk && this.talk.hasOwnProperty('title')) ?  this.talk.title : ""
+      },
+      name() {
+        return (this.talk && this.talk.hasOwnProperty('name')) ?  this.talk.name : ""
+      },
+      tags() {
+        let tags = []
+        if(this.talk && this.talk.hasOwnProperty('tags')){
+            let tagStr = this.talk.tags.slice(1).slice(0,-1)
+            tagStr.split(',').forEach(function(el) {
+              tags.push( el.replace( /"/g , "" ) )
+            })
+        }
+        return tags
+      }
+    }
+  }
+</script>
--- components/Event/TimeTable/index.vue
+++ components/Event/TimeTable/index.vue
@@ -2,19 +2,51 @@
 <style src="./table.sass" lang="sass" />
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+import SessionSummary from '~/components/Event/SessionSummary';
+
 export default {
   name: 'time-table',
   data: () => {
     return{
       isFixed: false,
       currentTab: "day-1",
+      rooms: {
+              "dayOne": ["ra","rb","rc","rd1","re","rf"],
+              "dayTwo": ["ra","rb","rc","rd2","rd3","re","rf"]
+              }
     }
   },
+  components: {
+    SessionSummary,
+  },
+  mounted(){
+    this.FETCH_TALK()
+  },
+  computed: {
+    ...mapGetters({
+      "talks": "talk_array",
+    })
+  },
   methods:{
-    changeTab: function (evt) {
+    ...mapActions({
+      FETCH_TALK: "FETCH_TALK"
+    }),
+    getTracks (day,no,room) {
+      let currentDayTracks = this.talks.filter( (item, index) => {
+                                                if(item.day === day) return true
+                                                })
+      if(currentDayTracks[0]){
+          return currentDayTracks.filter( (track) => {
+                                          if(track.no == no  && track.room_id == room ) return true
+                                          })[0]
+      }
+      return ""
+    },
+    changeTab (evt) {
       this.$data.currentTab = evt.currentTarget.getAttribute('data-tab')
     },
-    parallelScroll: function (evt, el) {
+    parallelScroll (evt, el) {
       let tableHeader = this.$refs[this.$data.currentTab]
       if(window.scrollY > (112+90)){
         const sLeft = el.scrollLeft
@@ -23,7 +55,7 @@ export default {
         tableHeader.scrollLeft = 0
       }
     },
-    fixedHead: function(evt, el) {
+    fixedHead (evt, el) {
       let tableHeader = this.$refs[this.$data.currentTab]
       if(tableHeader && (window.scrollY > (112+89))){
         this.$data.isFixed = true
